ff4b883e6da774ae7623fec1d150db66f4850e24
kayyjee <kylegilles@hotmail.com>
2014-11-29 18:32:29 -0800

    Changed DatabaseTest to if else format.

--- src/finaljk/Client.java
+++ src/finaljk/Client.java
@@ -4,28 +4,62 @@ import java.io.*;
 import java.net.*;
 
 class Client {
-
+    
+    private static DatagramPacket sendPacket;
+    private static DatagramSocket clientSocket;
+    private static InetAddress ClientIPAddress;
+    private static byte[] receiveData = new byte[1024];
+    private static byte[] sendData = new byte[1024];
+//int numofpacketsto sned. (window size)
     public static void main(String args[]) throws Exception {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket(7005);
-        InetAddress ClientIPAddress = InetAddress.getByName("localhost");
+        clientSocket = new DatagramSocket(7005);
+        ClientIPAddress = InetAddress.getByName("localhost");
         
         
         System.out.println(ClientIPAddress);
         
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
+        
         String sentence = inFromUser.readLine();
-        sendData = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ClientIPAddress, 7006);
+        String [] Packets =  CreatePackets(sentence);
+        int i;
+        for (i=0; i<10; i++){
+            sendData = Packets[i].getBytes();
+            sendPacket = new DatagramPacket(sendData, sendData.length, ClientIPAddress, 7006);
+            Send(sendPacket);
+            
+           }
         
         
         
-        clientSocket.send(sendPacket);
+        
+        
+        
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         clientSocket.receive(receivePacket);
         String modifiedSentence = new String(receivePacket.getData());
         System.out.println("FROM SERVER:" + modifiedSentence);
         clientSocket.close();
     }
+    
+    public static String[] CreatePackets(String message) {
+        String [] PacketArray = new String [10];
+        int i;
+        
+        for (i=0; i<10; i++){
+            PacketArray[i] = message + String.valueOf(i);
+            
+        }
+        return PacketArray;
+        
+    }
+    
+    public static void Send(DatagramPacket Packets) throws IOException {
+        
+      
+            
+            clientSocket.send(sendPacket);
+            //send(Packets);
+        }
+    
 }
